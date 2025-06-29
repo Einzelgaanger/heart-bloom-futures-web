7811dc30352827fb338c010ca89d4261f526befc
Einzelgaanger <binfred.ke@gmail.com>
2025-06-29 23:31:35 +0300

    002

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -163,7 +163,7 @@ const About = () => {
             <Card className="text-center hover:shadow-lg transition-shadow">
               <CardHeader>
                 <img 
-                  src="/Jotham.jpg" 
+                  src="/Joth.jpg" 
                   alt="Program Director" 
                   className="w-24 h-24 rounded-full mx-auto mb-4 object-cover"
                 />
